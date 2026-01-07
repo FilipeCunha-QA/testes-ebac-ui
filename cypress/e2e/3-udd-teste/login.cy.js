39ebe147ff881e23437080d2993bd63573a3b1f8
FilipeCunha-QA <filipecalmeida11@gmail.com>
2026-01-07 16:46:25 -0300

    primeiro teste ui

--- cypress/e2e/3-udd-teste/login.cy.js
+++ cypress/e2e/3-udd-teste/login.cy.js
@@ -2,12 +2,21 @@
 
 describe ('Funcionalidade: login', () =>{
     it('Deve fazer login com sucesso',() => {
-        cy.visit ('https://cursos.universidadedodota.com.br/auth/login')
-        cy.get('[name="Acesso[email]"]').type('filipecalmeida11@gmail.com')
-        cy.get('[name="Acesso[senha]"]').type('Igorfilipe1')
-        cy.get('.btn').click()
-        cy.get('https://cursos.universidadedodota.com.br/area/vitrine')
+        cy.visit ('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click ()
+        
+        cy.get('[data-test="title"]').should('contain','Products')
+    
        
+        it('deve exibir uma mensagem de erro ao inserir o usuario invalido', () => {
+            
+        cy.visit ('https://www.saucedemo.com/')
+        cy.get('[name="Acesso[email]"]').type('filipe@gmail.com')
+        cy.get('[name="Acesso[senha]"]').type('secret_sauce')
+        cy.get('.btn').click()
+        });
         
     })
 })
