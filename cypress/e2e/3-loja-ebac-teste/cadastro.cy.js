6832c746a5fdd44085d77432b6eb8df121dbe08b
FilipeCunha-QA <filipecalmeida11@gmail.com>
2026-01-26 12:42:42 -0300

    atualização final

--- cypress/e2e/3-loja-ebac-teste/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac-teste/cadastro.cy.js
@@ -45,12 +45,12 @@ it('Deve preencher cadastro com sucesso usando comando customizado', () => {
 });
 
 it('Deve fazer cadastro com sucesso usando Page Objects', () => {
-   cadastroPage.preencherCadastro('filipe113191@gmail.com', 'filipe123', 'filipe', 'Almeida')
+   cadastroPage.preencherCadastro('filipe555@gmail.com', 'filipe123', 'filipe', 'Almeida')
    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, filipe')
 });
 
 it('Deve validar mensagem ao tentar cadastrar sem preencher nome', () => {
-   cadastroPage.preencherCadastro('filipe810013@gmail.com', 'kk1234', 'Igor', '')
+   cadastroPage.preencherCadastro('filipi112@gmail.com', 'kk1234', 'Igor', '')
    cy.get('.woocommerce-error').should('contain', 'Sobrenome é um campo obrigatório.')
 
 
--- cypress/e2e/3-loja-ebac-teste/catalogo-produtos.cy.js
+++ cypress/e2e/3-loja-ebac-teste/catalogo-produtos.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress"/>
-import produtos from "../fixtures/produtos.json"
+import produtos from "../3-loja-ebac-teste/fixtures/produtos.json"
 describe('Funcionalidade: Busca no catalogo', () => {
     
     beforeEach(() => {
@@ -12,28 +12,48 @@ describe('Funcionalidade: Busca no catalogo', () => {
     cy.get('.product_title').should('contain', 'Kratos Gym Pant')
     
 });
+
 it('Deve fazer a busca do produto usando a massa de dados', () => {
-    cy.get('.search >').type(produtos[1].produtos)
-    cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
-    cy.get('.product_title').should('contain', produtos[1].produtos)
+    cy.fixture('produtos').then((prod) => {
+  expect(prod).to.be.an('array')
+  expect(prod[1]).to.have.property('Produtos')
+
+  cy.get('.search input[name="s"]')
+    .clear()
+    .type(prod[1].Produtos)
+})
 });
 it('deve fazer a busca de um produto usando o fixture', () => {
     cy.fixture('produtos').then((prod) => {
-    cy.get('.search >').type(prod[1].produtos)
+    cy.get('.search >').type(prod[1].Produtos)
     cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
-    cy.get('.product_title').should('contain', prod[1].produtos)
+    cy.get('.product_title').should('contain', prod[1].Produtos)
     })
 });
-it('Deve validar todos os produtos na lista', () => {
-  cy.fixture('produtos').then((prod) => {
-    prod.forEach((item) => {
-      cy.get('.search input[name="s"]').clear().type(item.produtos)
-    cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
-    cy.get('.search input[name="s"]').clear().type(item.produtos)
-cy.get('.product_title').should('contain', item.produtos)
+
+
+it('Deve validar todos os produtos da lista', () => {
+  cy.fixture('produtos').then((produtos) => {
+
+    cy.wrap(produtos).each((item) => {
+
+      cy.get('input[name="s"]:visible')
+        .first()
+        .should('be.enabled')
+        .clear()
+        .type(item.Produtos)
+
+      cy.get('button[type="submit"]:visible')
+        .first()
+        .click()
+
+      cy.get('.product_title')
+        .should('be.visible')
+        .and('contain.text', item.Produtos)
     })
   })
 })
+
 
 
 });
